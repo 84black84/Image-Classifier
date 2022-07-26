bb613a0eed0af7f1e96b56e07756d31a752814ac
Christos Mavrogiannis <Christos.Mavrogiannis@ottobockde.onmicrosoft.com>
2022-07-26 13:07:39 +0200

    create pipenv - install jupyter packages - start working on notebook

---
+++ my_cma_code.py
@@ -0,0 +1,8 @@
+# Import TensorFlow 
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import tensorflow_hub as hub
+
+# Ignore some warnings that are not relevant (you can remove this if you prefer)
+import warnings
+warnings.filterwarnings('ignore')
